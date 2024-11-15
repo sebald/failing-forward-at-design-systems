ec54c0124681ee4971e82dc4bc81a92dc9dcab15
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-16 00:10:26 +0100

    setup done-ish

--- scripts.js
+++ scripts.js
@@ -5,5 +5,40 @@ const deck = new Reveal({
   plugins: [RevealMarkdown],
   hash: true,
   controls: false,
+  progress: false,
+  // center: false,
+  transition: "none",
+  margin: 0.15,
+  width: 1200,
 });
+
 deck.initialize();
+
+/**
+ * Immediately reveal every fragment that has `[auto-load]`.
+ * Not that his only works if there are no non-auto load fragments
+ * in between.
+ */
+deck.addEventListener("slidechanged", (e) => {
+  if (
+    deck
+      .getCurrentSlide()
+      .querySelectorAll(`.fragment[data-autoload][data-fragment-index="0"]`)
+      .length
+  ) {
+    deck.nextFragment();
+  }
+});
+
+/**
+ * If there are fragments that have an autoload,
+ * transition to the previous slide if the fragment is hidden.
+ */
+deck.addEventListener("fragmenthidden", (e) => {
+  if (
+    e.fragment.hasAttribute("data-autoload") &&
+    e.fragment.getAttribute("data-fragment-index") === "0"
+  ) {
+    deck.prev();
+  }
+});
