693247788a19950540eda17387ffab1b0010ede4
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-17 00:17:36 +0100

    progress!

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 import Reveal from "reveal.js";
 import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Typewriter from "typewriter-effect/dist/core";
+
+const typewripter = el => {
+  // data-typewriter="|,100"
+  const [cursor = "", delay = 75] = el
+    .getAttribute("data-typewriter")
+    .split(",");
+
+  new Typewriter(el, {
+    strings: el.innerHTML,
+    autoStart: true,
+    cursor,
+    delay,
+  });
+};
 
 const deck = new Reveal({
   plugins: [RevealMarkdown],
@@ -14,14 +29,25 @@ const deck = new Reveal({
 
 deck.initialize();
 
-/**
- * Enter animation for elements
- */
-deck.on("slidechanged", (e) => {
+deck.on("slidechanged", e => {
   e.currentSlide
-    .querySelectorAll("[data-autoload]")
-    .forEach((el) => el.classList.add("visible"));
+    .querySelectorAll("[data-autoload]:not(.fragment)")
+    .forEach(el => el.classList.add("visible"));
   e.previousSlide
-    .querySelectorAll("[data-autoload]")
-    .forEach((el) => el.classList.remove("visible"));
+    ?.querySelectorAll("[data-autoload]")
+    .forEach(el => el.classList.remove("visible"));
+
+  e.currentSlide
+    .querySelectorAll("[data-autoload][data-typewriter]")
+    .forEach(el => typewripter(el));
+});
+
+deck.on("fragmentshown", e => {
+  if (e.fragment.hasAttribute("[data-typewriter]")) {
+    typewripter(e.fragment);
+  }
+
+  e.fragment
+    .querySelectorAll("[data-typewriter]")
+    .forEach(el => typewripter(el));
 });
