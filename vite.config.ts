b4972465d4083eea398a93fe040aae33d34aca18
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-20 15:58:42 +0100

    use assets folder as public folder to make revealjs find the images when build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     process.env.NODE_ENV === "production"
       ? "/failing-forward-at-design-systems"
       : "",
+  publicDir: "assets",
   server: {
     port: 3000,
   },
