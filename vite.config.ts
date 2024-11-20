e7dd7d599ca1e7433428a7fb20644766e9a57679
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-20 15:10:37 +0100

    base url test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   base:
     process.env.NODE_ENV === "production"
       ? "/failing-forward-at-design-systems"
-      : "/",
+      : "",
   server: {
     port: 3000,
   },
